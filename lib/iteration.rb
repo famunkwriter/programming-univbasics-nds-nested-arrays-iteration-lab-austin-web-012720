c844e8fb5c0b49ebba8d6da984f956d167fd1e57
Bretton B Holmes <famunkwriter@me.com>
2020-01-02 20:57:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,6 +44,7 @@ def total_even_pairs(src)
     total += src[row_index][0] + src[row_index][1]
   end
   row_index += 1
+end
   return total
 end
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
